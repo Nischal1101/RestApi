ec1fbdbd10868901d31745e8f0bb72097e987ec0
Nischal <nischalkafle123@gmail.com>
2023-01-19 18:23:04 +0545

    fixed pw leakage

--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const connectDB = () => {
   console.log("database connected");
-  console.log(process.env.URL);
   return mongoose.connect(process.env.URL);
 };
 module.exports = connectDB;
